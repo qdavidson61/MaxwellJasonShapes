6f9be0c3977928c18f6e39eaed421519714815ce
Jason Parker <120502006+jasonissuperawesome@users.noreply.github.com>
2023-02-09 12:47:47 -0500

    Updated with TriangularPrism implemented

--- src/TriangularPrism.java
+++ src/TriangularPrism.java
@@ -1,2 +1,22 @@
 public class TriangularPrism extends Rectangular{
+    private double a, b, c, height;
+
+    public TriangularPrism(double a, double b, double c, double height) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.height = height;
+    }
+
+    public double volume() {
+        double s = (a + b + c) / 2;
+        double baseArea = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        return baseArea * height;
+    }
+
+    public double surfaceArea() {
+        double s = (a + b + c) / 2;
+        double baseArea = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+        return baseArea * 2 + (a + b + c) * height;
+    }
 }
