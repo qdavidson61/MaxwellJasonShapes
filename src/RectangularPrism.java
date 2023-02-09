16b96cabc88f092c5b7a6c16116eab104ccccd42
Jason Parker <120502006+jasonissuperawesome@users.noreply.github.com>
2023-02-09 12:44:45 -0500

    Updated with RectangularPrism implemented

--- src/RectangularPrism.java
+++ src/RectangularPrism.java
@@ -1,2 +1,17 @@
 public class RectangularPrism extends Rectangular{
+    private double height, width, length;
+
+    public RectangularPrism(double height, double width, double length) {
+        this.height = height;
+        this.width = width;
+        this.length = length;
+    }
+
+    public double volume() {
+        return height * width * length;
+    }
+
+    public double surfaceArea() {
+        return 2 * (height * width + height * length + width * length);
+    }
 }
