2296768e70ea3c0d987fc4c33a74630713ce7c91
Jason Parker <120502006+jasonissuperawesome@users.noreply.github.com>
2023-02-09 13:52:33 -0500

    Need to add javadoc comments

--- src/TriangularPrism.java
+++ src/TriangularPrism.java
@@ -10,12 +10,11 @@ public class TriangularPrism extends Rectangular {
         super(height, base, width);
     }
 
-    @Override
+
     public double volume() {
         return height * (base * width) / 2;
     }
 
-    @Override
     public double surfaceArea() {
         double triangleArea = (base * width) / 2;
         return triangleArea * 2 + height * base + height * width;
