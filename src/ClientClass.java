d59b9151b95e92ba983474a229c51a317e10344d
Jason Parker <120502006+jasonissuperawesome@users.noreply.github.com>
2023-02-12 18:27:10 -0500

    Fixed formatting error

--- src/ClientClass.java
+++ src/ClientClass.java
@@ -20,40 +20,40 @@ public class ClientClass {
         //Cube:
         //Cube only takes one parameter called side, because all the side of a cube are equal. No need for redundancy
             Cube cu = new Cube(4);
-            System.out.println("Volume of Cube 'cu': " + df.format(cu.volume()) + " units cubed.");
-            System.out.println("Surface Area of Cube 'cu': " + df.format(cu.surfaceArea()) + " units squared.");
+            System.out.println("Volume of Cube: " + df.format(cu.volume()) + " units cubed.");
+            System.out.println("Surface Area of Cube: " + df.format(cu.surfaceArea()) + " units squared.");
             System.out.println();
 
 
         //Cone:
             Cone b = new Cone(5, 5);
-            System.out.println("Volume of Cone 'b':  " + df.format(b.volume()) + " units squared.");
-            System.out.println("Surface Area of cone 'b': " + df.format(b.surfaceArea()) + " units cubed.");
+            System.out.println("Volume of Cone:  " + df.format(b.volume()) + " units squared.");
+            System.out.println("Surface Area of cone: " + df.format(b.surfaceArea()) + " units cubed.");
             System.out.println();
 
         //Sphere:
         //NOTE: Sphere only takes radius, because you only need the radius to find the volume & SA of a sphere!
             Sphere s = new Sphere(6);
-            System.out.println("Surface Area of Sphere 's': " + df.format(s.surfaceArea()) + " units squared.");
-            System.out.println("Volume of Sphere 's': " + df.format(s.volume()) + " units cubed.");
+            System.out.println("Surface Area of Sphere: " + df.format(s.surfaceArea()) + " units squared.");
+            System.out.println("Volume of Sphere: " + df.format(s.volume()) + " units cubed.");
             System.out.println();
 
         //Cylinder:
             Cylinder c = new Cylinder(7, 6);
-            System.out.println("Volume of Cylinder 'c': " + df.format(c.volume()) + " units cubed.");
-            System.out.println("Surface Area of Cylinder 'c': " + df.format(c.surfaceArea()) + " units squared.");
+            System.out.println("Volume of Cylinder: " + df.format(c.volume()) + " units cubed.");
+            System.out.println("Surface Area of Cylinder: " + df.format(c.surfaceArea()) + " units squared.");
             System.out.println();
 
         //Rectangular Prism:
         RectangularPrism rp = new RectangularPrism(6, 7, 4);
-            System.out.println("Volume of Rectangular prism 'rp':  " + df.format(rp.volume()) + "units cubed");
-            System.out.println("Surface Area of Rectangular Prism 'rp': " + df.format(rp.surfaceArea()) + " units squared.");
+            System.out.println("Volume of Rectangular prism:  " + df.format(rp.volume()) + " units cubed");
+            System.out.println("Surface Area of Rectangular Prism: " + df.format(rp.surfaceArea()) + " units squared.");
             System.out.println();
 
         //Triangular Prism:
             TriangularPrism tp = new TriangularPrism(7, 3, 6);
-            System.out.println("Volume of Triangular prism 'tp': " + df.format(tp.volume()) + " units cubed");
-            System.out.println("Surface Area of Triangular Prism 'tp': " + df.format(tp.surfaceArea()) + " units squared.");
+            System.out.println("Volume of Triangular prism: " + df.format(tp.volume()) + " units cubed");
+            System.out.println("Surface Area of Triangular Prism: " + df.format(tp.surfaceArea()) + " units squared.");
             System.out.println();
 
         }
